bc0e1a1716f51a44573601debfdbc36d761fa0f9
Lu <66143267+TolulopeVerissimo@users.noreply.github.com>
2025-10-27 22:56:35 -0400

    add pdb file 3D 'ball & stick' model inside R3F

--- helix-frontend/src/pages/HomePage.tsx
+++ helix-frontend/src/pages/HomePage.tsx
@@ -1,5 +1,6 @@
 import { LinearSequence } from '../scene/renderers/LinearSequence';
 import { useQuery } from 'urql';
+import { PDBViewer } from '../scene/renderers/PDBViewer';
 
 const SequencesQuery = `
   query ListSequences {
@@ -24,10 +25,10 @@ export const HomePage = () => {
 
     return (
         <div>
+            {/* 2D view */}
             <h1 className="text-3xl font-bold mb-4 text-white">
                 {firstSequence?.name || 'DNA Sequence Viewer'}
             </h1>
-
             {firstSequence ? (
                 <LinearSequence sequence={firstSequence.data} />
             ) : (
@@ -35,6 +36,14 @@ export const HomePage = () => {
                     No sequences found in database.
                 </p>
             )}
+            {/* --- 3D Molecule Viewer --- */}
+            <h1 className="text-3xl font-bold mt-12 mb-4 text-white">
+                PDB Viewer
+            </h1>
+            <p className="text-lg text-gray-400 mb-6">
+                Click and drag to rotate. Use the scroll wheel to zoom.
+            </p>
+            <PDBViewer /> {/* <-- 2. Add the new component */}
         </div>
     );
 };
